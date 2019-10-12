0d8ccae73b7ef79a9993d7d0315ef1fc6aae8e8a
cseiasp <caroline.seilern@hotmail.com>
2019-10-12 09:58:10 +0100

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,36 @@
 class Zoo
 
+    attr_reader :name, :location, :animal
+
+    @@all = []
+
+    def initialize(location, name)
+        @name = name
+        @location = location
+        Zoo.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select {|animal| animal.zoo == self}
+    end
+
+    def animal_species
+        animals.map {|animal| animal.species}.uniq
+    end
+
+    def find_by_species(species)
+        animals.select {|animal| animal.species == species}
+    end
+
+    def animal_nicknames
+        animals.map {|animal| animal.name}
+    end
+
+    def self.find_by_location(location_name)
+        Zoo.all.select {|zoo| zoo.location == location_name}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,20 @@ require 'pry'
 
 
 #Test your code here
+zoo1 = Zoo.new("San Fran", "zoo1")
+zoo2 = Zoo.new("San Fran", "zoo2")
+
+animal1 = Animal.new("a", 55, "giraffe")
+animal2 = Animal.new("b", 54, "elephant")
+animal3 = Animal.new("c", 55, "giraffe")
+animal4 = Animal.new("d", 54, "elephant")
+animal5 = Animal.new("e", 54, "elephant")
+
+animal1.zoo = zoo1
+animal2.zoo = zoo1
+animal3.zoo = zoo2
+animal4.zoo = zoo2
+animal5.zoo = zoo2
 
 
 binding.pry
