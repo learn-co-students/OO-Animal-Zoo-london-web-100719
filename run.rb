d527f49d350df2b3bb411d933004fb16703e01aa
Tish Chungoora <tishal82@gmail.com>
2019-10-14 09:18:57 +0100

    Done.

--- run.rb
+++ run.rb
@@ -2,9 +2,19 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+animal1 = Animal.new("Tapir", 70, "Tim")
+animal2 = Animal.new("Giraffe", 400, "Dani")
+animal3 = Animal.new("Tiger", 300, "Tigroo")
+animal4 = Animal.new("Giraffe", 500, "'James")
+animal5 = Animal.new("Tapir", 100, "Fran")
+zoo1 = Zoo.new("London Zoo", "London")
+zoo2 = Zoo.new("Paris Zoo", "Paris")
 
-#Test your code here
-
+animal1.zoo = zoo1
+animal2.zoo = zoo1
+animal3.zoo = zoo2
+animal4.zoo = zoo2
+animal5.zoo = zoo1
 
 binding.pry
 puts "done"
