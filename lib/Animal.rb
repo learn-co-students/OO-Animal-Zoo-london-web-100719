e97a6313a42fa47f3a8b4e8c7dd672f7eecb0d08
flow1981 <florian.stagliano@gmx.de>
2019-10-15 09:40:22 +0100

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,28 @@
 class Animal
 
+    @@all=[]
+
+    attr_accessor :weight, :zoo
+    attr_reader :species, :nickname
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        Animal.all.select{|animal| animal.species==species}
+    end
+
+    def initialize(species,weight,nickname,zoo)
+        @species=species
+        @nickname=nickname
+        @weight=weight
+        @zoo=zoo
+
+        @@all << self
+        zoo.animals << self
+    end
+
+
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,26 @@
 class Zoo
 
+    @@all=[]
+    @animals=[]
+
+    attr_reader :name, :location, :animals
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_location(location)
+        self.all.select{|zoo| zoo.location==location}
+    end
+
+    def initialize(name, location)
+        @name=name
+        @location=location
+        @@all << self
+    end
+
+    def animal_species
+       animals.map {|animla| animal.species }.uniq
+    end
+
 end
